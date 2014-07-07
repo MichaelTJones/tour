0819d162826e13a60f1eb8c06268b65cee8ba805
Michael T Jones <mtj@google.com>
2014-07-07 13:55:46 -0700

    Update tour.go

--- tour.go
+++ tour.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
 	"time"
 
 	"github.com/MichaelTJones/walk"
 )
 
-// tour.go: Take a tour of one or more file hierarchies
+// tour.go: Benchmark tours of one or more file hierarchies
 
 func main() {
+	fmt.Printf("filepath.Walk(%v)\n", os.Args[1:])
+	for i := 0; i < 3; i++ {
+		filepathWalk()
+	}
+	fmt.Println()
+	fmt.Printf("walk.Walk(%v)\n", os.Args[1:])
+	for i := 0; i < 3; i++ {
+		walkWalk()
+	}
+}
+
+func filepathWalk() {
 	t0 := time.Now()
 	var files, bytes int64
 	var lock sync.Mutex
@@ -25,9 +38,34 @@ func main() {
 		return nil
 	}
 	for _, root := range os.Args[1:] {
-		walk.Walk(root, visitor)
+		err := filepath.Walk(root, visitor)
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
 	}
 	𝛥t := float64(time.Since(t0)) / 1e9
+	fmt.Printf("walked %d files containing %d bytes in %.3f seconds\n", files, bytes, 𝛥t)
+}
 
+func walkWalk() {
+	t0 := time.Now()
+	var files, bytes int64
+	var lock sync.Mutex
+	visitor := func(path string, info os.FileInfo, err error) error {
+		if err == nil {
+			lock.Lock()
+			files++
+			bytes += info.Size()
+			lock.Unlock()
+		}
+		return nil
+	}
+	for _, root := range os.Args[1:] {
+		err := walk.Walk(root, visitor)
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
+	}
+	𝛥t := float64(time.Since(t0)) / 1e9
 	fmt.Printf("walked %d files containing %d bytes in %.3f seconds\n", files, bytes, 𝛥t)
 }
